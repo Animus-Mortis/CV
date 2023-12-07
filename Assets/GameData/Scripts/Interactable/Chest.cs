033810cd9b3903bd630f719db1a865e04348eacc
Stanislav <66224457+Animus-Mortis@users.noreply.github.com>
2023-12-07 18:57:34 +0300

    Added chest Fire logic

---
+++ Assets/GameData/Scripts/Interactable/Chest.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using UnityEngine;
+
+namespace Game.Interactable
+{
+    public class Chest : MonoBehaviour, IInteractable
+    {
+        [SerializeField] private Animator anim;
+        [SerializeField] private Renderer goldRenderer;
+        [SerializeField] private float delayToDisolved;
+        [SerializeField] private float speedDisolved;
+
+        private bool isUsed;
+
+        public void Action()
+        {
+            isUsed = true;
+            anim.SetTrigger("Open");
+            StartCoroutine(GoldDisabled());
+        }
+
+        public bool CanUse()
+        {
+            return !isUsed;
+        }
+
+        private IEnumerator GoldDisabled()
+        {
+            yield return new WaitForSeconds(delayToDisolved);
+            Material material = goldRenderer.material;
+            float disolveValue = material.GetFloat("_Alfa");
+
+            while(disolveValue < 1)
+            {
+                disolveValue += speedDisolved * Time.deltaTime;
+                material.SetFloat("_Alfa", disolveValue);
+                yield return null;
+            }
+        }
+    }
+}
