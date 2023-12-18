6649a5915ade5afaf4b71eb2a962ff831cdd408b
Stanislav <66224457+Animus-Mortis@users.noreply.github.com>
2023-12-18 15:29:54 +0300

    Added a play icon to the video panel

--- Assets/GameData/Scripts/Interactable/Video.cs
+++ Assets/GameData/Scripts/Interactable/Video.cs
@@ -11,9 +11,17 @@ namespace Game.Interactable
     public class Video : MonoBehaviour, IInteractable
     {
         [SerializeField] private VideoPlayer videoPlayer;
+        [SerializeField] private Material videoMaterial;
         [SerializeField] private string nameFileClip;
 
         public Action<Video> playedAction;
+
+        private Renderer rendererThis;
+
+        private void Awake()
+        {
+            rendererThis = GetComponent<Renderer>();
+        }
 
         public void Action()
         {
@@ -34,6 +42,7 @@ namespace Game.Interactable
 
         private void PlayVideo()
         {
+            rendererThis.material = videoMaterial;
             string clipPath = Path.Combine(Application.streamingAssetsPath, nameFileClip);
             videoPlayer.url = clipPath;
             videoPlayer.Play();
